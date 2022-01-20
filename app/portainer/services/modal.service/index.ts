c97f5c54353bba2e35d758ac6f394da9566fc9df
Chaim Lev-Ari <chiptus@gmail.com>
2022-01-20 11:46:16 +0200

    refactor(access-control): create access-control-panel component [EE-2345]
    fixes [EE-2345]
    
    refactor(access-control): remove accessControlEnabled
    
    refactor(access-control): move details to component
    
    feat(access-control): create AccessControlPanelDetails
    
    refactor(access-control): move components
    
    refactor(users): use user interface
    
    refactor(access-control): use portainer types
    
    refactor(access-control): move widget to panel
    
    feat(access-control): calc state of panel
    
    refactor(access-control): show errors
    
    feat(access-control): show tooltip
    
    feat(access-control): add form WIP
    
    refactor(access-control): move button to panel
    
    refactor(access-control): refactor panel
    
    refactor(app/notifications): rename functions
    
    feat(app): gloabl error notify
    
    feat(access-control): access-control-panel component
    
    refactor(access-control): receive enum on create
    
    refactor(access-control): use react access-control panel
    
    fix(access-control): fix validation
    
    refactor(access-control): remove rest ops
    
    refactor(access-control): move types to module
    
    refactor(access-control): move form data to types
    
    refactor(access-control): use resource id
    
    refactor(access-control): build view model
    
    refactor(app): move queries
    
    refactor(access-control): move useOptions
    
    refactor(access-control): load teams and users
    
    fix(resourcecontrol): fix example api
    
    refactor(app): revert notifications names
    
    refactor(access-control): use regular for loop
    
    fix(access-control): fix tests
    
    refactor(access-control): check if user is team leader
    
    docs(access-control): sort types
    
    fix(access-control): toggle edit on success
    
    fix(access-control): fix conflicts
    
    refactor(access-control): resource control is optional

--- app/portainer/services/modal.service/index.ts
+++ app/portainer/services/modal.service/index.ts
@@ -3,7 +3,6 @@ import bootbox from 'bootbox';
 
 import {
   cancelRegistryRepositoryAction,
-  confirmAccessControlUpdate,
   confirmAsync,
   confirmDeassociate,
   confirmDeletion,
@@ -44,7 +43,6 @@ export function ModalServiceAngular() {
     confirmWebEditorDiscard,
     confirmAsync,
     confirm,
-    confirmAccessControlUpdate,
     confirmImageForceRemoval,
     cancelRegistryRepositoryAction,
     confirmDeletion,
