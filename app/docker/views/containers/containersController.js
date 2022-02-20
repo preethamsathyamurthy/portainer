59dfcf6b2e83d7b164ae8812041440bce1d6bf5c
Chaim Lev-Ari <chiptus@gmail.com>
2022-02-20 17:10:56 +0200

    refactor(containers): remove unused services

--- app/docker/views/containers/containersController.js
+++ app/docker/views/containers/containersController.js
@@ -1,7 +1,6 @@
 angular.module('portainer.docker').controller('ContainersController', ContainersController);
 
 /* @ngInject */
-function ContainersController($scope, ContainerService, Notifications, endpoint) {
-  $scope.offlineMode = endpoint.Status !== 1;
+function ContainersController($scope, endpoint) {
   $scope.endpoint = endpoint;
 }
